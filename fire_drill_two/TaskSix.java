80fc8bc2b854f8c6472d271e5c1f66e3acff3eff
ademesojosiah <josiahademeso@gmail.com>
2026-03-17 14:57:22 +0100

    feat: add fire drill two assessment

---
+++ fire_drill_two/TaskSix.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class TaskSix{
+
+	public static void main(String[] arg){
+
+
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter 10 scores and get the average");
+		
+
+		int i = 0;
+		float sum = 0;
+		int numberOfEvenNumbers = 0;
+		while(i++ < 10){
+			System.out.printf("Enter score %d: ", i);
+			int number = sc.nextInt();
+
+			if(number % 2 == 0){
+				sum = sum + number;
+				numberOfEvenNumbers = numberOfEvenNumbers + 1;
+			};
+		}
+
+		float average = sum/numberOfEvenNumbers;
+				
+		System.out.print("The average of the even numbers is  "+ average);
+
+
+		sc.close();
+
+	}
+
+}
